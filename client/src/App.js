7b94999a28ae40d21bb74e3f924e807ac5b37275
Jasani-Parth <82431676+Jasani-Parth@users.noreply.github.com>
2023-03-17 14:53:48 +0530

    header & logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,32 @@
 import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PageRender from "./PageRender";
-import Home from './pages/home'
-import Login from './pages/login'
+import Home from "./pages/home";
+import Login from "./pages/login";
 
-import Alert from './components/alert/Alert'
+import Alert from "./components/alert/Alert";
+import Header from "./components/Header";
 
-import { useSelector,useDispatch} from 'react-redux'
-import { refreshToken } from './redux/actions/authAction'
+import { useSelector, useDispatch } from "react-redux";
+import { refreshToken } from "./redux/actions/authAction";
 
 function App() {
-  const { auth } = useSelector(state => state)
-  const dispatch = useDispatch()
+  const { auth } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(refreshToken())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
 
   return (
     <Router>
-      <Alert/>
+      <Alert />
       <input type="checkbox" id="theme"></input>
       <div className="App">
         <div className="main">
+          {auth.token && <Header />}
           <Routes>
-          <Route exact path="/" Component={auth.token ? Home : Login} />
+            <Route exact path="/" Component={auth.token ? Home : Login} />
             <Route exact path="/:page" Component={PageRender} />
             <Route exact path="/:page/:id" Component={PageRender} />
           </Routes>
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,48 +1,80 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom'
-import { login } from '../redux/actions/authAction'
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../redux/actions/authAction";
+import { useDispatch, useSelector } from "react-redux";
 
 const Login = () => {
-  const initialState = { email: '', password: '' }
-  const [userData, setUserData] = useState(initialState)
-  const { email, password } = userData
-  const [typePass, setTypePass] = useState(false)
+  const initialState = { email: "", password: "" };
+  const [userData, setUserData] = useState(initialState);
+  const { email, password } = userData;
+  const [typePass, setTypePass] = useState(false);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { auth, alert } = useSelector((state) => state);
+  console.log(auth, alert);
 
-  const handleChangeInput = e => {
-    const { name, value } = e.target
-    setUserData({ ...userData, [name]: value })
-  }
+  useEffect(() => {
+    if (auth.token) navigate("/");
+  }, [auth.token, navigate]);
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    dispatch(login(userData))
-  }
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(userData));
+  };
   return (
     <div className="auth_page">
       <form onSubmit={handleSubmit}>
         <h3 className="text-uppercase text-center mb-4">Connect</h3>
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" onChange={handleChangeInput} value={email}/>
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            name="email"
+            aria-describedby="emailHelp"
+            onChange={handleChangeInput}
+            value={email}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
           <div className="pass">
-          <input type={typePass ? 'text':'password'} className="form-control" id="exampleInputPassword1" name="password" onChange={handleChangeInput} value={password}/>
-          <small onClick={()=>setTypePass(!typePass)}>{typePass ? 'Hide':'Show'}</small>
+            <input
+              type={typePass ? "text" : "password"}
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+              onChange={handleChangeInput}
+              value={password}
+            />
+            <small onClick={() => setTypePass(!typePass)}>
+              {typePass ? "Hide" : "Show"}
+            </small>
           </div>
         </div>
-        <button type="submit" className="btn btn-primary w-100" disabled={email && password ? false : true}>Login</button>
+        <button
+          type="submit"
+          className="btn btn-primary w-100"
+          disabled={email && password ? false : true}
+        >
+          Login
+        </button>
         <p className="my-2">
           You don't have an account ? <Link to="/register">Register Now</Link>
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
