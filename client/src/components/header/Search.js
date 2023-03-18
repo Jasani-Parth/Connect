d7b52f8dcd1e5eedd668334af7c86f5281d752cf
AnubhabRay31 <microsoftanubhab@gmail.com>
2023-03-19 01:42:57 +0530

    implemented profile update/edit

--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getDataAPI } from "../../utils/fetchData";
 import { GLOBALTYPES } from "../../redux/actions/globalTypes";
 import UserCard from "../UserCard";
-import LoadIcon from '../../images/loading.gif'
+import LoadIcon from "../../images/loading.gif";
 
 const Search = () => {
   const [search, setSearch] = useState("");
@@ -11,24 +11,23 @@ const Search = () => {
 
   const { auth } = useSelector((state) => state);
   const dispatch = useDispatch();
-  const [load, setLoad] = useState(false)
-
+  const [load, setLoad] = useState(false);
 
   const handleSearch = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (!search) return;
     try {
-      setLoad(true)
-      const res = await getDataAPI(`search?username=${search}`, auth.token)
-      setUsers(res.data.users)
-      setLoad(false)
+      setLoad(true);
+      const res = await getDataAPI(`search?username=${search}`, auth.token);
+      setUsers(res.data.users);
+      setLoad(false);
     } catch (error) {
       dispatch({
         type: GLOBALTYPES.ALERT,
         payload: { error: error.response.data.msg },
       });
     }
-  }
+  };
 
   const handleClose = () => {
     setSearch("");
@@ -43,33 +42,37 @@ const Search = () => {
           name="search"
           value={search}
           id="search"
+          title="Enter to Search"
           onChange={(e) =>
             setSearch(e.target.value.toLowerCase().replace(/ /g, ""))
           }
         />
         <div className="search_icon" style={{ opacity: search ? 0 : 0.3 }}>
           <span className="material-icons">search</span>
-          <span>Search</span>
+          <span>Enter to Search</span>
         </div>
         <div
-          className="close_search" onClick={handleClose}
-          style={{ opacity: users.length === 0 || search.length === 0 ? 0 : 1 }} >
+          className="close_search"
+          onClick={handleClose}
+          style={{ opacity: users.length === 0 || search.length === 0 ? 0 : 1 }}
+        >
           &times;
         </div>
 
-        <button type="submit" style={{ display: 'none' }}>Search</button>
+        <button type="submit" style={{ display: "none" }}>
+          Search
+        </button>
         {load && <img className="loading" src={LoadIcon} alt="loading" />}
 
         <div className="users">
           {search &&
-            users.map(user => (
-              <UserCard 
-              key={user._id} 
-              user={user} 
-              border="border" 
-              handleClose={handleClose}
+            users.map((user) => (
+              <UserCard
+                key={user._id}
+                user={user}
+                border="border"
+                handleClose={handleClose}
               />
-
             ))}
         </div>
       </form>
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,5 +4,6 @@ const userCtrl = require("../controllers/userCtrl");
 
 route.get("/search", auth, userCtrl.searchUser)
 route.get("/user/:id", auth, userCtrl.getUser)
+route.patch("/user", auth, userCtrl.updateUser)
 
 module.exports = route;
