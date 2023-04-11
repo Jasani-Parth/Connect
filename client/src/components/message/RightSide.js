5d4a4b8402382d34b88d48aaf5df5b88fa7e3905
Ameeya-ARS <ameeya.sethy@gmail.com>
2023-04-11 22:10:09 +0530

    Send/Recieve messages and image

--- client/src/components/message/RightSide.js
+++ client/src/components/message/RightSide.js
@@ -7,7 +7,7 @@ import Icons from "../Icons";
 import { GLOBALTYPES } from "../../redux/actions/globalTypes";
 import { imageShow } from "../../utils/mediaShow";
 import { imageUpload } from "../../utils/imageUpload";
-import { addMessage } from "../../redux/actions/messageAction";
+import { addMessage,getMessages } from "../../redux/actions/messageAction";
 import LoadIcon from "../../images/loading.gif";
 
 const RightSide = () => {
@@ -75,6 +75,15 @@ const RightSide = () => {
     SetLoadMedia(false);
     dispatch(addMessage({ msg, auth, socket }));
   };
+
+  useEffect(()=>{
+    if(id){
+      const getMessagesData = async () => {
+        await dispatch(getMessages({auth, id}))
+      }
+      getMessagesData()
+    }
+  },[id,dispatch,auth])
 
   return (
     <>
